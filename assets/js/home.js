34f28b01081f9bb758c8970ac02414461cfd03fc
prana <prakashrana9847067614@gmail.com>
2020-05-31 02:03:15 -0400

    First commit: navigation and hero image

---
+++ assets/js/home.js
@@ -0,0 +1,30 @@
+
+
+
+
+var navToggle = document.getElementById("nav-toggle")
+navToggle.addEventListener("click", function(){
+    console.log(navToggle.src)
+    var imgSrc = navToggle.src;
+    if (imgSrc.indexOf('hamburger.svg')!=-1) {
+        navToggle.src  = 'assets/icons/close.svg';
+        document.getElementById("overlay-div").classList.remove("close")
+        document.getElementById("overlay-div").classList.add("open")
+        document.getElementById("nav-div").classList.remove("close")
+        document.getElementById("nav-div").classList.add("open")
+
+    }
+     else {
+        navToggle.src  = 'assets/icons/hamburger.svg';
+        document.getElementById("overlay-div").classList.remove("open")
+        document.getElementById("overlay-div").classList.add("close")
+        document.getElementById("nav-div").classList.remove("open")
+        document.getElementById("nav-div").classList.add("close")
+ 
+
+   }
+
+    
+   
+
+});
